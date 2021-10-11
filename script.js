912513dad168a19c340cc0ec8714e299d06cd783
Evan Hardek <evanhardek@gmail.com>
2021-10-11 14:48:31 -0400

    Added Char Strings.

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 // TODO: Let Characters equal arrays 
 //   - Chars equal single strings in 4 arrays
+lowerStr = 'a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r-s-t-u-v-w-x-y-z';
+upperStr= 'A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-S-T-U-V-W-X-Y-Z';
+numStr = '1-2-3-4-5-6-7-9-0';
+specialStr = "!-#-$-%-(-)-*-+-.-/-:-;-<-=->-?-@-[-\-]-^-`-{-|-}-~";
+
+//   - Use split() separate Char strings 
+
+// TODO: Take User's Chosen Characters and Password Length 
+//   - Use Prompt() for User For Password Length
+//     - Password Length must be 8 - 128
+//     - Password Length must be A number 
+//   - Use Confirm() as boolean for other Chars
+//     - We must let the Chars boolean from confirm()
+//   - Return function as User's Chosen Characters
+
+// TODO: Write Generate Password Function
+//   - Take Chosen variables and set them in to an array for iteration
+//    - Use Switch Statement to process Char prompt and Confirm
+//   - Randomize the index of iteration (Could be separate function)
+//   - Combine Chosen Char Arrays using switch case
+//   - Select at least one of each chosen Character Array 
 //   - Use split() separate Char strings 
 
 // TODO: Take User's Chosen Characters and Password Length 
